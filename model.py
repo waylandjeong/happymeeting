ad8eda3ac025a8bb4915539c05b31665012e2cea
Wayland Jeong <wjeong@hpe.com>
2018-04-22 22:53:07 -0700

    Added class for a user database.

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 from peewee import *
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 import datetime
 
 proxy = Proxy()
@@ -17,7 +19,21 @@ class Post(BaseModel):
     score = IntegerField()
 
 
+class User(BaseModel):
+    id = PrimaryKeyField()
+    username = CharField()
+    email = CharField()
+    password_hash = TextField()
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 def initialize_db():
     proxy.connect()
     proxy.create_tables([Post], safe=True)
+    proxy.create_tables([User], safe=True)
 
